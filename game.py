2b525d0497010c8690d31d36003c6f8deed25bef
Soroush <soroushmpour@gmail.com>
2019-04-30 23:27:53 +0430

    minor changes

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import engine
 from engine import color
 from engine import  clear
-commander = engine.resistanceEngine()
 while True:
+    commander = engine.resistanceEngine()
     clear()
     print(color.BOLD + color.CYAN + '\t'*2 + 'Welcome to PyResistance' + color.END)
     print(color.CYAN + '\t'*2 + 'you should have 5 to 10 players, use following commands to add players and start the game.' + color.END)
@@ -10,6 +10,7 @@ while True:
         command = input('<start> the game or <add_players> ?')
         if command == '<start>':
             commander.start()
+            break
         elif command == '<add_players>':
             commander.setupNewPlayer()
         else:
